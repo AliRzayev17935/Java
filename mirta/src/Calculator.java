82a4d805c78ce4a0fd2c726226f35ad106b512cd
AliRzayev17935 <151455834+AliRzayev17935@users.noreply.github.com>
2024-01-19 15:15:01 +0400

    Update Calculator.java

--- mirta/src/Calculator.java
+++ mirta/src/Calculator.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 
-
 public class Calculator {
     public static void main(String[] args) {
-        int riyaz, kimya, fizika, musiqi, azeri, tarix;
-        double ortalama;
-        String yorum;
         Scanner input = new Scanner(System.in);
-        System.out.print("Riyazziyat:");
-        riyaz = input.nextInt();
-        System.out.print("Kimya:");
-        kimya = input.nextInt();
-        System.out.print("Fizika:");
-        fizika = input.nextInt();
-        System.out.print("Musiqi:");
-        musiqi = input.nextInt();
-        System.out.print("Azernbaycan Dili:");
-        azeri = input.nextInt();
-        System.out.print("Tarix:");
-        tarix = input.nextInt();
-        ortalama = (riyaz + kimya + fizika + musiqi +azeri+ tarix) / 6.0;
-        System.out.print("Ortalama:");
-        System.out.println(ortalama);
-        yorum = (ortalama > 60) ? "Kecdin" : "Kesildin";
-        System.out.print(yorum);
+        int n1, n2, select, res = 0;
+        System.out.print("Ilk Ededi Daxil Edin :");
+        n1 = input.nextInt();
+        System.out.print("Ikinci Ededi Daxil Edin :");
+        n2 = input.nextInt();
+        System.out.print(" 1- Toplama\n2- Çıxma\n3- Vurma\n4- Bölmə\nLazimli Emeliyyati secin:");
+        select = input.nextInt();
+        if (select == 1){
+            res = n1+n2;
+        }
+        else if (select == 2){
+            res = n1 - n2;
+        }
+        else if (select == 3){
+            res = n1 * n2;
+        }
+        else if (select == 4){
+            res = n1 / n2;
+        }
+        else {
+            System.out.print("Daxil Edilen Reqem Duzgun Deyil");
+        }
+        System.out.print("Netice :" + res);
     }
 }
